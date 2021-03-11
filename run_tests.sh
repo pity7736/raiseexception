fb03239a555c8e3f8f232deffcaab1f039594054
Julián Cortés <julian.cortes77@pm.me>
2021-03-10 22:03:27 -0500

    chore: removed docker stop

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 docker-compose -f docker-compose-production.yml -f docker-compose.yml run --rm raise_exception  pytest -s -vvv --cov=raiseexception --cov-report term-missing tests
-docker stop raiseexception_db_1

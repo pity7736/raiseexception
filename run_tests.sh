cc6086f954e6fc59554adfe1ab30381739f7db48
Julián Cortés <julian.cortes77@pm.me>
2021-03-10 22:23:31 -0500

    ci: list

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pwd
-ls
+ls -al
 docker-compose -f docker-compose-production.yml -f docker-compose.yml run --rm raise_exception  pytest -s -vvv --cov=raiseexception --cov-report term-missing tests

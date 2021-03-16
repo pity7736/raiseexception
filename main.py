6972b578f6278e47bf596e42b43fdaecd17eecf9
Julián Cortés <julian.cortes77@pm.me>
2021-03-15 23:28:04 -0500

    feat: get login view

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import uvicorn
 
+from raiseexception import settings
+
 if __name__ == '__main__':
     uvicorn.run(
         app='raiseexception:app',
         host='0.0.0.0',
         port=8000,
+        reload=settings.DEBUG
     )
--- tests/integration_tests/auth/views/test_login.py
+++ tests/integration_tests/auth/views/test_login.py
@@ -3,9 +3,18 @@ from starlette.testclient import TestClient
 from raiseexception import app
 
 
-def test_get(db_connection):
+def test_get():
     client = TestClient(app=app)
     response = client.get('/auth/login')
 
     assert response.status_code == 200
-    assert '' in response.text
+    assert '<form id="login" action="/auth/login" method="post">' \
+           in response.text
+    assert '<label for="username">Username:</label>' in response.text
+    assert '<input id="username" name="username" type="text" required ' \
+           'autofocus>' in response.text
+    assert '<label for="password">Password:</label>' in response.text
+    assert '<input id="password" name="password" type="password" ' \
+           'required>' in response.text
+    assert '<input type="submit" value="login">' in response.text
+    assert '</form>' in response.text
